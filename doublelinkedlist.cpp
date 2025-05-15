1a4b28f3a538882592487cf8afb232c354c29be0
sulthann1 <sulthanaawaliya155@gmail.com>
2025-05-15 18:42:50 +0700

    menambahkan void addNote

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -4,28 +4,33 @@ using namespace std;
 
 class Node
 {
-    public :
-        int noMhs;
-        Node *next;
-        Node *prev;
-
+public:
+    int noMhs;
+    Node *next;
+    Node *prev;
 };
 
 class DoubleLinkedList
 {
-    private :
-        Node *START;
+private:
+    Node *START;
 
-    public :
-        DoubleLinkedList()
-        {
-            START = NULL;
-        }
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
 
-        void addNode()
-        {
-            int nim;
-            cout << "\nEnter the roll number of student :";
-            cin >> nim; 
-        }
-}
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of student :";
+        cin >> nim;
+
+        // Step 1 : Allocate memory for new node
+        Node *newNode = new Node();
+
+        // Step 2 : Insert at beginning if list is empty
+        newNode -> noMhs = nim;
+    }
+};
