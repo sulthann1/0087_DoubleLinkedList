17557c7485230d49b8210d181d718581f4959669
sulthann1 <sulthanaawaliya155@gmail.com>
2025-05-15 18:53:43 +0700

    menambahkan if

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -30,7 +30,38 @@ public:
         // Step 1 : Allocate memory for new node
         Node *newNode = new Node();
 
-        // Step 2 : Insert at beginning if list is empty
+        // Step 2 : Assign  value to the data field
         newNode -> noMhs = nim;
+        
+        //Step 3 : Insert at beginning if list is empty or nim is
+        if (START == NULL || nim <= START -> noMhs )
+        (
+            if (START != NULL && nim  == START -> noMhs )
+            {
+                cout << "\nDuplicate  number not allowed" << endl; 
+                return;
+            }
+            // Step 4 : newNode.next = START
+            newNode -> next = START;
+
+            //Step 5 : START.prev = newNode (if START exist)
+            {
+            if (START !=NULL)
+            START -> prev = newNode;
+
+            // Step 6 : newNode.prev = NULL
+            newNode -> prev = NULL;
+            // Step 7 : START = newNode
+            START = newNode;
+            return;
+            }
+
+            //insert in between node
+            // Step 8 : locate position for insertion
+
+            Node *current = START;
+            while (current -> next != NULL && current -> next -> noMhs < nim)
+
+        )
     }
 };
